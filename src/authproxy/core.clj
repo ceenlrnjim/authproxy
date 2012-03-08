3580da4c410d0fdbba282b39b7e57f383024de20
James Kirkwood <kirkwojl@hotmail.com>
2012-03-08 13:00:49 -0500

    initial setup

---
+++ src/authproxy/core.clj
@@ -0,0 +1,1 @@
+(ns authproxy.core)
---
+++ test/authproxy/test/core.clj
@@ -0,0 +1,6 @@
+(ns authproxy.test.core
+  (:use [authproxy.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
