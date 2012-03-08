c02b7b840c7e5c1da13349beca2c949f82b6d05b
James Kirkwood <kirkwojl@hotmail.com>
2012-03-08 16:24:39 -0500

    directly coding to jetty api setup

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject authproxy "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.0.2"]
-                 [ring/ring-jetty-adapter "1.0.2"]]
+                 [org.mortbay.jetty/jetty-embedded "6.1.26"]
+                 ;[ring/ring-core "1.0.2"]
+                 ;[ring/ring-jetty-adapter "1.0.2"]'
+                 ]
   :main authproxy.core
   )
--- src/authproxy/core.clj
+++ src/authproxy/core.clj
@@ -1,12 +1,33 @@
 (ns authproxy.core
-    (:use ring.adapter.jetty))
+    ;(:use ring.adapter.jetty)
+    (:import [org.mortbay.jetty Server])
+    (:import [org.mortbay.jetty.servlet Context ServletHolder])
+    (:import [javax.servlet Servlet])
+    )
 
 (def mapping { "source" "http://someotherhost:port/target" })
 
+(defn proxy-servlet
+    "constructs and returns the servlet that proxies requests"
+    []
+    (proxy [javax.servlet.http.HttpServlet] []
+        (doGet [req resp]
+            (.println (.getOutputStream resp) "<html><body><p>Hello World</p></body></html>"))))
+
+(defn -main [& args]
+  (let [server (Server. 8080)
+        ctx (Context. server "/" Context/SESSIONS)]
+    (.addServlet ctx (ServletHolder. (proxy-servlet)) "/*")
+    (.start server)))
+
+(comment
 (defn handler [req]
+  (println (.getClass req))
+  (println req)
   { :status 200
     :headers {"content-type" "text/html"}
     :body "Hello World"})
 
 (defn -main [& args]
-  (run-jetty #'handler {:port 8081} ))
+  (run-jetty #'handler {:port 8080} ))
+)
