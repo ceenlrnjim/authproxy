0e59ae29787dd9386f5bff6816b69756d1f1701c
James Kirkwood <kirkwojl@hotmail.com>
2012-03-08 15:47:41 -0500

    setup

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject authproxy "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-core "1.0.2"]
+                 [ring/ring-jetty-adapter "1.0.2"]]
+  :main authproxy.core
+  )
--- src/authproxy/core.clj
+++ src/authproxy/core.clj
@@ -1,1 +1,12 @@
-(ns authproxy.core)
+(ns authproxy.core
+    (:use ring.adapter.jetty))
+
+(def mapping { "source" "http://someotherhost:port/target" })
+
+(defn handler [req]
+  { :status 200
+    :headers {"content-type" "text/html"}
+    :body "Hello World"})
+
+(defn -main [& args]
+  (run-jetty #'handler {:port 8081} ))
