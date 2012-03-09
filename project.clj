ae49ca4946be1187f217d5464b1853ed3ee4cb4e
James Kirkwood <kirkwojl@hotmail.com>
2012-03-09 08:55:04 -0500

    added logging and stubbed out manual auth handling

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
                  ;[org.mortbay.jetty/jetty-embedded "6.1.26"]
                  [ring/ring-core "1.0.2"]
                  [ring/ring-jetty-adapter "1.0.2"]
-                 [clj-http "0.1.3"]]
+                 [log4j/log4j "1.2.16"]
+                 [org.clojure/tools.logging "0.1.0"]
+                ; [clj-http "0.1.3"]
+                 ]
   :main authproxy.core
   )
--- src/authproxy/core.clj
+++ src/authproxy/core.clj
@@ -1,33 +1,32 @@
 (ns authproxy.core
-    (:use ring.adapter.jetty)
-    (:require [clj-http.client :as client])
-  (:import [java.net URL URLConnection HttpURLConnection])
-    ;(:import [org.mortbay.jetty Server])
-    ;(:import [org.mortbay.jetty.servlet Context ServletHolder])
-    ;(:import [javax.servlet Servlet])
-    )
+  (:use ring.adapter.jetty)
+  (:require [clojure.tools.logging :as log])
+  ;(:require [clj-http.client :as client])
+  (:import [java.net URL URLConnection HttpURLConnection]))
 
-; TODO: may want to have host values here as well
+; TODO: may want to have host values here as well - 
+; TODO: probably want a setting to shut off automatic auth for certain hosts
 (def mapping { "rfc.thinkerjk.com:8081" "http://www.ietf.org" 
                "nyt.thinkerjk.com:8081" "http://www.nytimes.com"
                "tcmanager.thinkerjk.com:8081" "http://localhost:8080"}) 
-              
 
-(comment
-(defn proxy-servlet
-    "constructs and returns the servlet that proxies requests"
-    []
-    (proxy [javax.servlet.http.HttpServlet] []
-        (doGet [req resp]
-            (.println (.getOutputStream resp) "<html><body><p>Hello World</p></body></html>"))))
+; Vars (threadlocals) to be bound to username and *password* for a specific request
+(def ^:dynamic *username* nil)
+(def ^:dynamic *password* nil)
 
-(defn -main [& args]
-  (let [server (Server. 8080)
-        ctx (Context. server "/" Context/SESSIONS)]
-    (.addServlet ctx (ServletHolder. (proxy-servlet)) "/*")
-    (.start server)))
-)
-;---------------------------------------------------------------------------------------------
+; Credential store for all logged in users
+; TODO: ultimately want to capture user credentials and save to their session
+(def credentials (atom {"tomcat" "tomcat"}))
+
+(defn- register-authenticator
+  "Registers a default authenticator that pulls username and password from the thread local
+  bindings set up when the request is initiated."
+  []
+  (java.net.Authenticator/setDefault
+    (proxy [java.net.Authenticator] []
+      (getPasswordAuthentication []
+        (log/debug "Creating password authentication for " *username* "/" *password*)
+        (java.net.PasswordAuthentication. *username* (.toCharArray *password*))))))
 
 (defn- host-value
   "Returns the host header value from the specified request"
@@ -42,7 +41,10 @@
 (defn- target-url
   "Returns the appropriate target url from the mapping"
   [req]
-  (str (get mapping (host-value req)) (:uri req) (query-string req)))
+  (let [host (host-value req)
+        target (str (get mapping (host-value req)) (:uri req) (query-string req))]
+    (log/debug "Generating target " target " for host " host)
+    target))
 
 (defn- header-list-to-string
   [value-list]
@@ -79,21 +81,41 @@
     ;(.setDoOutput conn true)
     (.connect conn)
     (let [headers (convert-header-map (.getHeaderFields conn))]
-      (println headers)
       { :status (.getResponseCode conn)
         :headers headers
         :body (return-stream conn) })))
 
+; TODO: support SPNEGO, NTLM, etc,
+; Starting with just Basic auth
+; TODO: currently not doing anything - seeing if the java authenticator will do everything I need
+; TODO: do I need to do this or will the java.net.authenticator take care of everything?
+;(defn- proxy-auth
+;  "Handles an authentication request returned by a proxied web location.
+;  req is the original request to the proxy
+;  resp is the response generated from the proxied web site"
+;  [req resp]
+;  (log/debug "Handling auth request...")
+;  resp)
+
+(defn- proxy-response
+  "Provide any logic to handle any responses that require logic, otherwise just return back to client"
+  [req resp]
+  resp)
+  ;(cond (= (:status resp) 401) (proxy-auth req resp)
+  ;      :else resp))
+
 (defn handler [req]
-  (println req)
+  (log/debug "Received request: " req)
   ;{ :status 200 })
-  (let [target (target-url req)
-        resp (issue-request req target)]
-    ; TODO: switch to log4j and add appropriate tracing
-    (println "Routing to target " target)
-    (println (if (= (:status resp) 401) "Auth challenge detected" "No auth, passing to client"))
-    resp))
+  ; TODO: look at session, get user identifier and then use that to look up credentials in the credentials atom
+  (binding [*username* "tomcat"
+            *password* (get @credentials "tomcat")]
+    (log/debug "Credentials initialized: " *username* "/" *password*)
+    (let [target (target-url req)
+          resp (issue-request req target)]
+      (proxy-response req resp))))
    
 
 (defn -main [& args]
+  (register-authenticator)
   (run-jetty #'handler {:port 8081} ))
