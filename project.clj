c13eefe79cf878776e8f3b9d0ea38c6c0b758af1
James Kirkwood <kirkwojl@hotmail.com>
2012-03-08 19:48:50 -0500

    first rudimentary proxy with mapping

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject authproxy "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.mortbay.jetty/jetty-embedded "6.1.26"]
-                 ;[ring/ring-core "1.0.2"]
-                 ;[ring/ring-jetty-adapter "1.0.2"]'
-                 ]
+                 ;[org.mortbay.jetty/jetty-embedded "6.1.26"]
+                 [ring/ring-core "1.0.2"]
+                 [ring/ring-jetty-adapter "1.0.2"]
+                 [clj-http "0.1.3"]]
   :main authproxy.core
   )
--- src/authproxy/core.clj
+++ src/authproxy/core.clj
@@ -1,12 +1,16 @@
 (ns authproxy.core
-    ;(:use ring.adapter.jetty)
-    (:import [org.mortbay.jetty Server])
-    (:import [org.mortbay.jetty.servlet Context ServletHolder])
-    (:import [javax.servlet Servlet])
+    (:use ring.adapter.jetty)
+    (:require [clj-http.client :as client])
+    ;(:import [org.mortbay.jetty Server])
+    ;(:import [org.mortbay.jetty.servlet Context ServletHolder])
+    ;(:import [javax.servlet Servlet])
     )
 
-(def mapping { "source" "http://someotherhost:port/target" })
+(def mapping { "localhost:8080" { "/a/b" "http://www.ietf.org" 
+                                  "/nyt" "http://www.nytimes.com"} } ) 
+              
 
+(comment
 (defn proxy-servlet
     "constructs and returns the servlet that proxies requests"
     []
@@ -19,15 +23,32 @@
         ctx (Context. server "/" Context/SESSIONS)]
     (.addServlet ctx (ServletHolder. (proxy-servlet)) "/*")
     (.start server)))
+)
 
-(comment
+(defn- host-value
+  "Returns the host header value from the specified request"
+  [req]
+  (get (:headers req) "host"))
+
+(defn- uri-target
+  "Returns the first value from the map where the key matches the beginning of the uri for the specified request"
+  [host-map req]
+  (first (filter #(.startsWith (:uri req) (get % 0)) host-map)))
+
+(defn- target-url
+  "Returns the appropriate target url from the mapping"
+  [req]
+  (let [host-map (get mapping (host-value req))
+        target (uri-target host-map req)]
+    (str (get target 1) ; the host name from the nested map
+         (.substring (:uri req) (.length (get target 0)))))) ; strip the uri key off the target url
+
 (defn handler [req]
-  (println (.getClass req))
   (println req)
-  { :status 200
-    :headers {"content-type" "text/html"}
-    :body "Hello World"})
+  ;{ :status 200 })
+  (let [target (target-url req)]
+    (println "Target " target)
+   (client/get target (dissoc (:headers req) "host"))))
 
 (defn -main [& args]
   (run-jetty #'handler {:port 8080} ))
-)
